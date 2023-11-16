de584b658929ca7b5413a32673ae2d60e582ad80
J.Schuster <2123206@stud.hs-mannheim.de>
2023-11-16 21:01:36 +0100

    fixed small error

--- Listen und Schleifen/SchleifenMitAufforderung.py
+++ Listen und Schleifen/SchleifenMitAufforderung.py
@@ -2,7 +2,7 @@ choice = "j"
 while choice == "j":
     dividend = int(input("Geben sie den Dividend ein: "))
     divisor = int(input("Geben sie den Divisor ein: "))
-    while(dividend > divisor):
+    while(dividend >= divisor):
         dividend -= divisor
     print("Der rest beträgt " + str(dividend))
     choice = input("Wollen sie das Programm wiederholen? (j): ")
